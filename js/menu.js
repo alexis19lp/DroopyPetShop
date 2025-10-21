b5f0d42bb2e48504f6b081a661a6445cb8d91dd5
alexis19lp <alexisgimenez95@hotmail.com>
2025-10-21 20:56:13 -0300

    agregué boton menú con media query

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    const expanded = hamburger.getAttribute("aria-expanded") === "true";
+    hamburger.setAttribute("aria-expanded", !expanded);
+  });
